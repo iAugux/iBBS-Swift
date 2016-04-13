4e72e304ce9f905cc6772f55be5318053281427c
iAugus <iAugux@gmail.com>
2016-04-13 12:02:14 +0800

    localhost

--- iBBS/IBBSAPIClient.swift
+++ iBBS/IBBSAPIClient.swift
@@ -14,7 +14,8 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 
-let APIRootURL = "http://127.0.0.1/YABBS/index.php/Home/Api/"
+let APIRootURL = "http://192.168.1.100/YABBS/index.php/Home/Api/"
+//let APIRootURL = "http://127.0.0.1/YABBS/index.php/Home/Api/"
 //let APIRootURL = "http://obbs.sinaapp.com/index.php/Home/Api/"
 
 
